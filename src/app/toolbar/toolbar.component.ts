2422049fbead3ef727abdaf48cebe12104abaa26
Thiago Honorato <thiagohbhonorato@gmail.com>
2021-09-01 15:31:50 -0300

    Initial toolbar

---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'pm-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent {}
